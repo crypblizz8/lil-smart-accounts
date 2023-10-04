b1e437481bba45d64d50405de90bc3e6c3c62be9
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2023-10-04 17:57:00 +0200

    chore: movbile fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,14 +28,20 @@ createWeb3Modal({ wagmiConfig, projectId, chains });
 export default function Home() {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <main className="flex min-h-screen flex-col items-center p-24">
+      <main className="flex min-h-screen flex-col items-center p-4 sm:p-24">
         <div className="flex flex-col items-center">
           <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
             Lil Smart Accounts
           </h5>
           <w3m-button />
           <p className="mt-2">Fallback Vanity Address:</p>
-          <p>{process.env.NEXT_PUBLIC_VANITY_ADDRESS}</p>
+          <p>
+            {process.env.NEXT_PUBLIC_VANITY_ADDRESS?.substring(0, 6) +
+              "..." +
+              process.env.NEXT_PUBLIC_VANITY_ADDRESS?.substring(
+                process.env.NEXT_PUBLIC_VANITY_ADDRESS.length - 4
+              )}
+          </p>
         </div>
         <div className="grid grid-cols-1 gap-4 mb-4 sm:mb-8 sm:grid-cols-2">
           {data.map((item: ItemProps, index: number) => (
